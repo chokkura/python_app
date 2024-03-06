491edf0eefd8d56075a9f3c0e38208dd7bb0e8a0
chokkura <pjgrooei@msn.com>
2024-03-06 23:51:08 +0900

    add: views for news

--- mhlw_news/urls.py
+++ mhlw_news/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
   path("", views.index, name="index"),
+  path("<int:news_id>/", views.detail, name="detail"),
+  path("<int:news_id>/results/", views.results, name="results"),
+  path("<int:news_id>/vote/", views.vote, name="vote"),
 ]
--- mhlw_news/views.py
+++ mhlw_news/views.py
@@ -1,5 +1,22 @@
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404
+
+from .models import News
 
 
+# 厚生労働省のニュースを一覧表示
 def index(request):
-  return HttpResponse("厚生労働省のニュースをかみくだくサイトです")
+    latest_news_list = News.objects.order_by("-pub_date")[:5]
+    context = {"latest_news_list": latest_news_list,}
+    return render(request, "mhlw_news/index.html", context)
+
+def detail(request, news_id):
+    news = News.objects.get(pk=news_id)
+    return render(request, "mhlw_news/detail.html", {"news": news})
+
+def results(request, news_id):
+    response = "%sの記事です"
+    return HttpResponse(response % news_id)
+
+def vote(request, news_id):
+    return HttpResponse("%sの記事に投票します" % news_id)
