2d6568134bf81a1cd048b5c0104b6685fc699d3d
chokkura <pjgrooei@msn.com>
2024-03-07 23:44:06 +0900

    refactor: views,urls

--- mhlw_news/views.py
+++ mhlw_news/views.py
@@ -1,22 +1,45 @@
+
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, Http404
+from django.urls import reverse
+from django.views import generic
+from django.db.models import F
 
-from .models import News
+from .models import Choice, News
 
 
-# 厚生労働省のニュースを一覧表示
-def index(request):
-    latest_news_list = News.objects.order_by("-pub_date")[:5]
-    context = {"latest_news_list": latest_news_list,}
-    return render(request, "mhlw_news/index.html", context)
+class IndexView(generic.ListView):
+    # 厚生労働省のニュースを一覧表示
+    template_name = "mhlw_news/index.html"
+    context_object_name = "latest_news_list"
 
-def detail(request, news_id):
-    news = News.objects.get(pk=news_id)
-    return render(request, "mhlw_news/detail.html", {"news": news})
-
-def results(request, news_id):
-    response = "%sの記事です"
-    return HttpResponse(response % news_id)
+    def get_queryset(self):
+        """ 直近5記事を表示"""
+        return News.objects.order_by("-pub_date")[:5]
+    
+class DetailView(generic.DetailView):
+    model = News
+    template_name = "mhlw_news/detail.html"
+    
+class ResultsView(generic.DetailView):
+    model = News
+    template_name = "mhlw_news/results.html"
+                  
 
 def vote(request, news_id):
-    return HttpResponse("%sの記事に投票します" % news_id)
+    news = get_object_or_404(News, pk=news_id)
+    try:
+        selected_choice = news.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # 投票フォームを再表示する
+        return render(
+            request, "mhlw_news/detail.html",
+            { "news": news,
+             "error_message": "意見を選択していません"},
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        # POSTデータの対処に成功すればHttpResponseRedirectで戻る
+        # これによりBackボタンを押してデータが2回送信されるのを防げる
+        return HttpResponseRedirect(reverse("mhlw_news:results", args=(news.id,)))
