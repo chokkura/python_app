7a5dad9ed0496bcdb94df5afd957e5eec38f11f8
chokkura <pjgrooei@msn.com>
2024-03-12 22:26:09 +0900

    update: help_text for model

--- mhlw_news/models.py
+++ mhlw_news/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.utils import timezone
 
 class News(models.Model):
-    # 厚生労働省のニュースオブジェクト
+    # 厚生労働省のニュース
     #   field: 健康・医療, 福祉・介護, 雇用・労働, 年金, その他
     #   pub_date: ニュースがアップロードされた日時 
     @admin.display(
@@ -21,8 +21,8 @@ class News(models.Model):
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
     
     title = models.CharField(max_length=30)
-    field = models.CharField(max_length=20)
-    summary = models.CharField(max_length=200)
+    field = models.CharField(max_length=20, help_text='分野')
+    summary = models.CharField(max_length=200, help_text='記事の要約')
     pub_date = models.DateTimeField("date published")
 
 class Choice(models.Model):
@@ -32,5 +32,5 @@ class Choice(models.Model):
       return self.choice_text
 
     news = models.ForeignKey(News, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=200, help_text='記事への意見')
     votes = models.IntegerField(default=0)
