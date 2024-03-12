9dced67318ec0337b77d981f76987a564921044d
chokkura <pjgrooei@msn.com>
2024-03-12 19:32:03 +0900

    update:url_path for toppage

--- myPythonSite/urls.py
+++ myPythonSite/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path("mhlw_news/", include("mhlw_news.urls")),
     path("admin/", admin.site.urls),
+    path("", include("mhlw_news.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
